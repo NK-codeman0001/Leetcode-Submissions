ec42cebae1d9178d56de16e7cb5ff73f57b7694b
Neeraj Kumar <66253834+NK-codeman0001@users.noreply.github.com>
2024-03-31 20:16:12 +0530

    Runtime: 335 ms (faster than 5.52%), Memory: 67.7 MB (less than 99.88%) - GitSync

---
+++ 53-maximum-subarray/53. Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans=INT_MIN,n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            ans=max(ans,sum);
+            if(sum<0) sum=0;
+        }
+        return ans;
+    }
+};
