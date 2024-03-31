f761f5a423e449dce2a6f5eee0bf17bc0d920234
Neeraj Kumar <66253834+NK-codeman0001@users.noreply.github.com>
2024-03-31 20:33:33 +0530

    Runtime: 57 ms (faster than 34.73%), Memory: 23.1 MB (less than 61.25%) - GitSync

---
+++ 1162-as-far-from-land-as-possible/1162. As Far from Land as Possible.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        
+        vector <vector <int>> cell(r, vector <int>(c, -1));
+        queue <pair<int,int>> bfs;
+        
+        // set the water cells to 0 and land cells to -1
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1) {
+                    cell[i][j] = 0;
+                    bfs.push({i, j});
+                }
+            }
+        }
+        
+        int dist = 1;
+        while (!bfs.empty()) {
+            int len = bfs.size();
+            
+            for (int k = 0; k < len; k++) {
+                
+                //for each cell check its 4 boundary cells
+                //if it is not visited, increase its distance from land cell by 1
+                pair <int, int> curr = bfs.front(); bfs.pop();
+                int i = curr.first, j = curr.second;
+                
+                // top cell
+                if (i > 0 && cell[i - 1][j] == -1) {
+                    cell[i - 1][j] = dist;
+                    bfs.push({i - 1, j});
+                }
+                //left cell
+                if (j > 0 && cell[i][j - 1] == -1) {
+                    cell[i][j - 1] = dist;
+                    bfs.push({i, j - 1});
+                }
+                //bottom cell
+                if (i < r - 1 && cell[i + 1][j] == -1) {
+                    cell[i + 1][j] = dist;
+                    bfs.push({i + 1, j});
+                }
+                // right cell
+                if (j < c - 1 && cell[i][j + 1] == -1) {
+                    cell[i][j + 1] = dist;
+                    bfs.push({i, j + 1});
+                }               
+            }
+            
+            // after one complete round increase the distance by 1
+            dist += 1;
+        }
+        
+        // find the maximum distance among the cells
+        int m = INT_MIN;
+        for (auto v: cell) {
+            for (int i: v) {
+                if (i > m) {
+                    m = i;
+                }
+            }
+        }
+        
+        // if maximum value is 0, 
+        // which means the cell with the closest land cell is a land cell
+        // return -1
+        if (m == 0) {
+            return -1;
+        }
+        
+        return m;
+    }
+};
